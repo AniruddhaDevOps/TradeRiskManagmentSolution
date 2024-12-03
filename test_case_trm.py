73b8349fd05d7f7ede05fdc2572d9b0f8efa13cd
AniruddhaDevOps <das.aniruddha2021@gmail.com>
2024-12-03 19:01:14 +0100

    Update test_case_trm.py
    commited

--- test_case_trm.py
+++ test_case_trm.py
@@ -6,17 +6,17 @@ from traderiskmangment import BlackScholeForwardPrice
 def option_trading():
     trade_date = '2022-11-23'
     expiry_date = '2023-05-10'
-    stock_price = 19
+    spot_price = 19
     strike_price = 17
     risk_free = 0.005
     sigma = 0.3
-    return BlackScholeForwardPrice(trade_date, expiry_date, stock_price, strike_price, risk_free, sigma)
+    return BlackScholeForwardPrice(trade_date, expiry_date, spot_price, strike_price, risk_free, sigma)
 
 def test_at_the_money(option_trading):
     expected_call_options = 1.39597
     expected_put_options = 1.35699
 
-    option_trading.stock_price = 17
+    option_trading.spot_price = 17
 
     assert pytest.approx(option_trading.call_options(), 0.001) == expected_call_options
     assert pytest.approx(option_trading.put_options(), 0.001) == expected_put_options
@@ -32,7 +32,7 @@ def test_out_the_money(option_trading):
     expected_call_options = 0.54279
     expected_put_options = 2.50381
 
-    option_trading.stock_price = 15
+    option_trading.spot_price = 15
 
     assert pytest.approx(option_trading.call_options(), 0.001) == expected_call_options
     assert pytest.approx(option_trading.put_options(), 0.001) == expected_put_options
